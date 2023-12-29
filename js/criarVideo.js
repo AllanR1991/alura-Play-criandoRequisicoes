229d0d076cf243ea6c37ad8a5770192a7845a628
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2023-12-28 23:47:31 -0300

    Realizado criação de videos

---
+++ js/criarVideo.js
@@ -0,0 +1,19 @@
+import {conectaApi} from "./conectaApi.js";
+
+const formulario = document.querySelector("[data-formulario]");
+
+async function criarVideo(evento){
+    const imagem = document.querySelector("[data-imagem]").value;
+    const url = document.querySelector("[data-url]").value;
+    const titulo = document.querySelector("[data-titulo]").value;
+    const descricao = Math.floor(Math.random() * 10).toString();
+
+    evento.preventDefault();
+
+    await conectaApi.criaVideo(titulo,descricao,url,imagem);
+    
+    //Este comando faz com que a pagina de envio concluido seja exibida;
+    window.location.href = "../pages/envio-concluido.html"
+}
+
+formulario.addEventListener("submit", evento => criarVideo(evento));
